e4f3049f4bab79d668a1607b3f94df8f8d16b79b
xiedong <xiedong11@aliyun.com>
2019-07-17 18:21:03 +0800

    集成jpush

---
+++ app/src/main/java/com/zhuandian/common/jpush/JPushReceiver.java
@@ -0,0 +1,11 @@
+package com.zhuandian.common.jpush;
+
+import cn.jpush.android.service.JPushMessageReceiver;
+
+/**
+ * desc :
+ * author：xiedong
+ * date：2019/7/17
+ */
+public class JPushReceiver extends JPushMessageReceiver {
+}
