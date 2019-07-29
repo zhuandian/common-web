bdf04a1d1b2493f2bb75f5da92ae195b0d044699
xiedong <xiedong11@aliyun.com>
2019-07-29 09:03:54 +0800

    修改设置页点击事件处理

--- app/src/main/java/com/zhuandian/common/business/fragment/SettingFragment.kt
+++ app/src/main/java/com/zhuandian/common/business/fragment/SettingFragment.kt
@@ -3,7 +3,9 @@ package com.zhuandian.common.business.fragment
 import android.app.Activity
 import android.view.View
 import com.zhuandian.common.R
+import com.zhuandian.common.base.BaseActivity
 import com.zhuandian.common.base.BaseFragment
+import com.zhuandian.common.business.AppIntroActivity
 import com.zhuandian.common.business.FeedBackActivity
 import com.zhuandian.common.business.login.LoginActivity
 import kotlinx.android.synthetic.main.fragment_setting.*
@@ -17,21 +19,34 @@ import org.jetbrains.anko.yesButton
  * author：xiedong
  * date：2019/7/18
  */
-class SettingFragment : BaseFragment() {
+class SettingFragment : BaseFragment(), View.OnClickListener {
+
     override fun getLayoutId(): Int = R.layout.fragment_setting
 
     override fun setUpView() {
         iv_back.visibility = View.GONE
         iv_share.visibility = View.GONE
         tv_title.text = "设置"
-        rl_feed_back.setOnClickListener { (activity as Activity).startActivity<FeedBackActivity>() }
+        rl_feed_back.setOnClickListener(this)
         tv_version.text = getAppVersion()
-        rl_new_version.setOnClickListener {
-            (activity as Activity).alert("当前已经最新版本", "升级提醒") { yesButton { it.cancel() } }.show()
-        }
-        btn_logout.setOnClickListener {
-            (activity as Activity).startActivity<LoginActivity>()
-            activity!!.finish()
+        rl_new_version.setOnClickListener(this)
+        btn_logout.setOnClickListener(this)
+        rl_help.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.rl_feed_back ->
+                (activity as Activity).startActivity<FeedBackActivity>()
+            R.id.rl_new_version ->
+                (activity as Activity).alert("当前已经最新版本", "升级提醒") { yesButton { it.cancel() } }.show()
+            R.id.btn_logout -> {
+                (activity as Activity).startActivity<LoginActivity>()
+                activity!!.finish()
+            }
+            R.id.rl_help ->
+                (activity as Activity).startActivity<AppIntroActivity>()
+
         }
     }
 
