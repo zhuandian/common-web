ad6b9356c46b9fcf9cebfc08d74a1b4dc6eaf894
xiedong <xiedong11@aliyun.com>
2019-07-30 18:11:21 +0800

    修改标题显示

--- app/src/main/java/com/zhuandian/common/business/HomeListItemDetailActivity.kt
+++ app/src/main/java/com/zhuandian/common/business/HomeListItemDetailActivity.kt
@@ -4,7 +4,11 @@ import com.bumptech.glide.Glide
 import com.zhuandian.common.R
 import com.zhuandian.common.base.BaseActivity
 import com.zhuandian.common.entity.HomeList
+import kotlinx.android.synthetic.main.activity_home_list_item_detail.*
 import kotlinx.android.synthetic.main.item_home_list.*
+import kotlinx.android.synthetic.main.item_home_list.iv_img
+import kotlinx.android.synthetic.main.item_home_list.tv_content
+import kotlinx.android.synthetic.main.item_home_list.tv_time
 
 class HomeListItemDetailActivity : BaseActivity() {
     override fun getLayoutId(): Int = R.layout.activity_home_list_item_detail
@@ -14,7 +18,7 @@ class HomeListItemDetailActivity : BaseActivity() {
         var data: HomeList = intent.getSerializableExtra("data") as HomeList
         Glide.with(this).load(data.imgUrl).into(iv_img)
         tv_time.text = data.time
-        tv_title.text = data.title
+        tv_item_title.text = data.title
         tv_content.text = data.content
 
     }
